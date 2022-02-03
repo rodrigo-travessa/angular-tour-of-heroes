055098c4271c5db218ae8de33b4490e6bb8d1e32
Rodrigo Travessa <rodrigosigiliao@outlook.com>
2022-02-03 14:19:54 -0300

    Implemented asynchronous requests of heroes using service, instead of synchronous.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';  
+
 
 
 
@@ -11,12 +12,18 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
   
-  constructor() { }
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
