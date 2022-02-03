62c659251412a84b3a2bd728b3aa1f9836868c14
Rodrigo Travessa <rodrigosigiliao@outlook.com>
2022-02-03 14:07:15 -0300

    Starting Service implementation

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -4,14 +4,6 @@ export const HEROES: Hero[] = [
     {id: 11, name: 'Dr.Evil'},
     {id: 12, name: 'Dr.Distraught'},
     {id: 13, name: 'Dr.EvilLaugther'},
-    {id: 14, name: 'Dr.Bane'},
-    {id: 15, name: 'Dr.Bad'},
-    {id: 16, name: 'Dr.NotsoGood'},
-    {id: 17, name: 'Dr.AntiVillain'},
-    {id: 18, name: 'Dr.Chicago'},
-    {id: 19, name: 'Dr.SuperMe'},
-    {id: 20, name: 'Dr.NotSoBad'},
-    {id: 21, name: 'Dr.BadAtBeingGood'},
-    {id: 22, name: 'Dr.RunningOut'},
-    {id: 23, name: 'Dr.RanOutOfNames'}
+    {id: 14, name: 'Dr.Bane'}
+  
 ]
